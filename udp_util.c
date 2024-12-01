1f193373edf7498a1717287db056bb76b591e93b
carvalhof <carvalhof.br@gmail.com>
2024-11-30 22:00:11 -0400

    wip

--- udp_util.c
+++ udp_util.c
@@ -1,18 +1,18 @@
 #include "udp_util.h"
 
-// Shuffle the UDP source port array
-void shuffle(uint16_t* arr, uint32_t n) {
-	if(n < 2) {
-		return;
-	}
+// // Shuffle the UDP source port array
+// void shuffle(uint16_t* arr, uint32_t n) {
+// 	if(n < 2) {
+// 		return;
+// 	}
 
-	for(uint32_t i = 0; i < n - 1; i++) {
-		uint32_t j = i + rte_rand() / (UINT64_MAX / (n - i) + 1);
-		uint16_t tmp = arr[j];
-		arr[j] = arr[i];
-		arr[i] = tmp;
-	}
-}
+// 	for(uint32_t i = 0; i < n - 1; i++) {
+// 		uint32_t j = i + rte_rand() / (UINT64_MAX / (n - i) + 1);
+// 		uint16_t tmp = arr[j];
+// 		arr[j] = arr[i];
+// 		arr[i] = tmp;
+// 	}
+// }
 
 // Create and initialize the Control Blocks for all flows
 void init_blocks() {
@@ -21,22 +21,22 @@ void init_blocks() {
 
 	// choose UDP source port for all flows
 	uint16_t src_udp_port;
-	uint16_t ports[nr_flows];
+	uint16_t src_ports[nr_flows];
 	for(uint32_t i = 0; i < nr_flows; i++) {
-		ports[i] = rte_cpu_to_be_16((i % (nr_flows/nr_servers)) + 1);
+		src_ports[i] = rte_cpu_to_be_16((i % nr_flows) + 1);
 	}
 
-	// shuffle port array
-	shuffle(ports, nr_flows);
+	// // shuffle port array
+	// shuffle(ports, nr_flows);
 
 	for(uint32_t i = 0; i < nr_flows; i++) {
-		src_udp_port = ports[i];
+		src_udp_port = src_ports[i];
 
 		control_blocks[i].src_addr = src_ipv4_addr;
 		control_blocks[i].dst_addr = dst_ipv4_addr;
 
 		control_blocks[i].src_port = src_udp_port;
-		control_blocks[i].dst_port = rte_cpu_to_be_16(dst_udp_port + (i % nr_servers));
+		control_blocks[i].dst_port = rte_cpu_to_be_16(dst_udp_port);
 
 		control_blocks[i].flow_mark_action.id = i;
 		control_blocks[i].flow_queue_action.index = i;
